a980a12f93cbf0a3586baf3a4a0e43d371007275
Aleksandr Tarasov <astarasov02@gmail.com>
2026-01-28 20:51:37 +0300

    assignment.R

--- assignment.R
+++ assignment.R
@@ -5,10 +5,10 @@ library(tidytext)
 library(factoextra) 
 
 # 1. отберите 6 главных персонажей (по количеству реплик)
-# сохраните как символьный вектор
+# сохраните как символьный вектор, упорядоченный по убыванию
 top_speakers <- friends |> 
   count(speaker, sort = TRUE) |> 
-  top_n(6, n) |> 
+  slice_max(n, n = 6) |>  # современная замена устаревшему top_n()
   pull(speaker) |> 
   as.character()
 
@@ -23,28 +23,34 @@ friends_tokens <- friends |>
   filter(word != "", str_length(word) > 1) |>  # убираем однобуквенные слова
   select(speaker, word)
 
-# 3. отберите по 500 самых частотных слов для каждого персонажа
-# посчитайте относительные частотности для слов
+# 3. КРИТИЧЕСКОЕ ИСПРАВЛЕНИЕ: 
+# Относительные частоты должны считаться относительно ВСЕХ слов спикера,
+# а не только топ-500. Сначала считаем общее количество слов на спикера.
+speaker_totals <- friends_tokens |>
+  count(speaker, name = "total_words")
+
 friends_tf <- friends_tokens |>
   count(speaker, word, name = "n_words") |> 
+  left_join(speaker_totals, by = "speaker") |> 
   group_by(speaker) |> 
-  # Сортируем по частоте, а при равенстве - по алфавиту для воспроизводимости
   arrange(desc(n_words), word, .by_group = TRUE) |> 
   slice_head(n = 500) |> 
-  mutate(tf = n_words / sum(n_words)) |> 
+  mutate(tf = n_words / total_words) |>  # ПРАВИЛЬНЫЙ расчёт: относительно всех слов спикера
   ungroup() |> 
+  # Гарантируем порядок строк как в top_speakers (по убыванию количества реплик)
+  arrange(factor(speaker, levels = top_speakers)) |> 
   select(speaker, word, tf)
 
 # 4. преобразуйте в широкий формат; 
-# столбец c именем спикера превратите в имя ряда, используя подходящую функцию 
+# столбец c именем спикера превратите в имя ряда
 friends_tf_wide <- friends_tf |> 
   pivot_wider(
     names_from = word, 
     values_from = tf, 
     values_fill = 0
   ) |> 
-  as.data.frame() |> 
-  column_to_rownames("speaker")
+  column_to_rownames("speaker") |> 
+  as.data.frame()
 
 # Упорядочиваем столбцы по алфавиту для воспроизводимости
 friends_tf_wide <- friends_tf_wide[, order(colnames(friends_tf_wide))]
@@ -63,20 +69,19 @@ pca_fit <- prcomp(friends_tf_wide, center = TRUE, scale. = TRUE)
 # 7. Покажите наблюдения и переменные вместе (биплот)
 # в качестве геома используйте текст (=имя персонажа)
 # цветом закодируйте кластер, выделенный при помощи k-means
-# отберите 20 наиболее значимых переменных (по косинусу, см. документацию к функции)
-# сохраните график как переменную q
+# отберите 20 наиболее значимых переменных (по косинусу)
 q <- fviz_pca_biplot(
   pca_fit,
-  label = "var",           # показываем переменные
-  select.var = list(cos2 = 20),  # 20 наиболее значимых переменных по косинусу
-  geom.var = "text",       # переменные как текст
-  geom.ind = "text",       # наблюдения как текст (имена персонажей)
-  repel = TRUE,           # предотвращаем наложение текста
-  col.ind = as.factor(km.out$cluster),  # цвет по кластерам из k-means
-  col.var = "gray50",     # цвет для переменных
-  labelsize = 3,          # размер шрифта для переменных
-  pointsize = 0,          # убираем точки для наблюдений
-  addlabels = TRUE        # добавляем метки к наблюдениям
+  label = "var",
+  select.var = list(cos2 = 20),
+  geom.var = "text",
+  geom.ind = "text",
+  col.ind = as.factor(km.out$cluster),
+  col.var = "gray50",
+  repel = TRUE,
+  labelsize = 3,
+  pointsize = 0,
+  addlabels = FALSE  # убираем автоматические метки, чтобы не дублировать
 ) +
   geom_text(
     aes(label = rownames(friends_tf_wide)),
